eb15a3377ec2e34384fc95ae454046ac7cbdeb1a
Melvin Lammerts <melvin.lammerts@yahoo.com>
2015-02-04 14:23:12 +0100

    Initial Commit

---
+++ BurpToggle/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  BurpToggle
+//
+//  Created by Melvin Lammerts on 04/02/15.
+//  Copyright (c) 2015 Melvin Lammerts. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
